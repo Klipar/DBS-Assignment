70729ebce47db94046bb4dc45b44ef9539711f37
Arseniy Malyuk <arsenmaliuk12@gmail.com>
2026-04-26 21:11:02 +0200

    Add schema SQL file

---
+++ schema.sql
@@ -0,0 +1,134 @@
+﻿-- 1. ENUM TYPES
+
+CREATE TYPE staff_position AS ENUM ('Manager', 'Cashier', 'Cleaner', 'Technician', 'Security');
+CREATE TYPE screening_language AS ENUM ('EN', 'UA', 'SK');
+CREATE TYPE film_age_group AS ENUM ('G', 'PG', 'PG-13', 'R', 'NC-17');
+
+
+-- 2. TABLES WITHOUT FOREIGN KEYS
+
+-- staffs table
+CREATE TABLE staffs (
+                        id SMALLSERIAL PRIMARY KEY,
+                        name VARCHAR(32) NOT NULL,
+                        surname VARCHAR(32) NOT NULL,
+                        age SMALLINT CHECK (age >= 15),
+                        position staff_position NOT NULL,
+                        working_since DATE DEFAULT CURRENT_DATE
+);
+
+-- halls table
+CREATE TABLE halls (
+                       id SMALLSERIAL PRIMARY KEY,
+                       name VARCHAR(16) UNIQUE NOT NULL,
+                       capacity SMALLINT NOT NULL CHECK (capacity > 0)
+);
+
+-- films table
+CREATE TABLE films (
+                       id SMALLSERIAL PRIMARY KEY,
+                       name VARCHAR(256) NOT NULL,
+                       duration SMALLINT NOT NULL CHECK (duration > 0),
+                       age_group film_age_group NOT NULL
+);
+
+-- actors table
+CREATE TABLE actors (
+                        id SMALLSERIAL PRIMARY KEY,
+                        name VARCHAR(32) NOT NULL,
+                        surname VARCHAR(32) NOT NULL
+);
+
+-- genres table
+CREATE TABLE genres (
+                        id SMALLSERIAL PRIMARY KEY,
+                        name VARCHAR(32) UNIQUE NOT NULL
+);
+
+-- studios table
+CREATE TABLE studios (
+                         id SMALLSERIAL PRIMARY KEY,
+                         name VARCHAR(32) UNIQUE NOT NULL
+);
+
+
+-- 3. TABLES WITH FOREIGN KEYS
+
+-- shifts table
+CREATE TABLE shifts (
+                        id SMALLSERIAL PRIMARY KEY,
+                        description VARCHAR(512),
+                        start_time TIMESTAMP NOT NULL,
+                        end_time TIMESTAMP NOT NULL CHECK (end_time > start_time),
+                        staff_id SMALLINT NOT NULL REFERENCES staffs(id)
+                            ON DELETE RESTRICT
+                            ON UPDATE CASCADE
+);
+
+-- screenings table
+CREATE TABLE screenings (
+                            id SMALLSERIAL PRIMARY KEY,
+                            start_time TIMESTAMP NOT NULL,
+                            language screening_language NOT NULL,
+                            film_id SMALLINT NOT NULL REFERENCES films(id)
+                                ON DELETE RESTRICT
+                                ON UPDATE CASCADE,
+                            hall_id SMALLINT NOT NULL REFERENCES halls(id)
+                                ON DELETE RESTRICT
+                                ON UPDATE CASCADE
+);
+
+-- sales table
+CREATE TABLE sales (
+                       id SMALLSERIAL PRIMARY KEY,
+                       customer_name VARCHAR(32) NOT NULL,
+                       customer_surname VARCHAR(32) NOT NULL,
+                       customer_age SMALLINT CHECK (customer_age >= 0),
+                       screening_id SMALLINT NOT NULL REFERENCES screenings(id)
+                           ON DELETE RESTRICT
+                           ON UPDATE CASCADE
+);
+
+-- shifts_screenings
+CREATE TABLE shifts_screenings (
+                                   id SMALLSERIAL PRIMARY KEY,
+                                   shift_id SMALLINT NOT NULL REFERENCES shifts(id)
+                                       ON DELETE RESTRICT
+                                       ON UPDATE CASCADE,
+                                   screening_id SMALLINT NOT NULL REFERENCES screenings(id)
+                                       ON DELETE RESTRICT
+                                       ON UPDATE CASCADE
+);
+
+-- actors_films
+CREATE TABLE actors_films (
+                              id SMALLSERIAL PRIMARY KEY,
+                              actor_id SMALLINT NOT NULL REFERENCES actors(id)
+                                  ON DELETE RESTRICT
+                                  ON UPDATE CASCADE,
+                              film_id SMALLINT NOT NULL REFERENCES films(id)
+                                  ON DELETE RESTRICT
+                                  ON UPDATE CASCADE
+);
+
+-- films_genres
+CREATE TABLE films_genres (
+                              id SMALLSERIAL PRIMARY KEY,
+                              film_id SMALLINT NOT NULL REFERENCES films(id)
+                                  ON DELETE RESTRICT
+                                  ON UPDATE CASCADE,
+                              genre_id SMALLINT NOT NULL REFERENCES genres(id)
+                                  ON DELETE RESTRICT
+                                  ON UPDATE CASCADE
+);
+
+-- films_studios
+CREATE TABLE films_studios (
+                               id SMALLSERIAL PRIMARY KEY,
+                               film_id SMALLINT NOT NULL REFERENCES films(id)
+                                   ON DELETE RESTRICT
+                                   ON UPDATE CASCADE,
+                               studio_id SMALLINT NOT NULL REFERENCES studios(id)
+                                   ON DELETE RESTRICT
+                                   ON UPDATE CASCADE
+);
